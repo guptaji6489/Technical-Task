b81e0f25d4d650704cd3139835973b13355e6e1b
guptaji6489 <guptaji6489@gmail.com>
2020-08-08 22:57:50 +0530

    running localhost:3000

---
+++ api/routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../db');
+/* GET home page. */
+
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/create', function(req, res) {
+  console.log("xyz......");
+ 
+  // var database = req.body.hits;
+  // db.collection("customers").insertOne(database, function(err, res) {
+  //   if (err) throw err;
+  // console.log("1 document inserted");
+  console.log(req.body.hits);
+  res.send("successful");
+});
+// });
+
+
+module.exports = router;
